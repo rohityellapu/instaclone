e3f5af03ffe2934edfd1e4c027d9fc9d5ea15e45
rohityellapu <rohithyellapu24@gmail.com>
2022-12-14 19:13:42 +0530

    added final touches

--- src/PostView/Header.jsx
+++ src/PostView/Header.jsx
@@ -1,14 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Header.css';
 export default function Header({ setForm }) {
     return (
         <header>
             <Link to='/'>
                 <div className="logo">
-                    <img src={ require('../images/logos/image1.jpg') } alt="logo" />
+                    <img src='https://img.icons8.com/clouds/512/instagram-new--v3.png' alt="logo" />
                 </div>
             </Link>
+            <div className="logo-name">
+                <h1>InstaClone</h1>
+                <h4>by Rohit Yellapu</h4>
+            </div>
             <div className="camera" onClick={ () => setForm(prev => !prev) }>
-                <img src={ require('../images/logos/camera.png') } alt="camera" />
+                <img src="https://img.icons8.com/ios-filled/512/camera.png" alt="camera" />
             </div>
         </header>
     )
